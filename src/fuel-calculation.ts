57863fb686d5d1ea8365c15740bc85282e3bc2be
JoshuaNeely <joshua.neely@adtran.com>
2019-12-02 15:43:15 -0600

    init

---
+++ src/fuel-calculation.ts
@@ -0,0 +1,3 @@
+export function calculate_fuel() {
+  return 42;
+}
---
+++ tests/sample.spec.ts
@@ -0,0 +1,8 @@
+import { calculate_fuel } from "src/fuel-calculation";
+
+describe('calculate_fuel', function() {
+  it('should do stuff', function() {
+    const result = calculate_fuel();
+    expect(result).toBe(42);
+  });
+});
