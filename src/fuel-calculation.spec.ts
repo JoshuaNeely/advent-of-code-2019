c39c47b9639554257a6b22577609bcb86ee1f2eb
JoshuaNeely <joshua.neely@adtran.com>
2019-12-02 16:36:29 -0600

    problem 1 solved

---
+++ src/fuel-calculation.spec.ts
@@ -0,0 +1,23 @@
+import { calculateFuel, reducer } from "src/fuel-calculation";
+
+const testData = [
+  {mass: 12, fuel: 2},
+  {mass: 14, fuel: 2},
+  {mass: 1969, fuel: 654},
+  {mass: 100756, fuel: 33583},
+]
+
+describe('calculateFuel', function() {
+  for (const datum of testData) {
+    it(`should correctly calculate fuel from ${datum.mass} mass`, function() {
+      const result = calculateFuel(datum.mass);
+      expect(result).toBe(datum.fuel);
+    });
+  }
+
+  it(`should correctly sum up fuel calculations`, function() {
+    const masses = [12, 1969];
+    const result = masses.reduce(reducer, 0);
+    expect(result).toBe(calculateFuel(12) + calculateFuel(1969));
+  });
+});
