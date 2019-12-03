308289a5a6e039023a6e6eb14d51f8fe207ae84b
JoshuaNeely <joshua.neely@adtran.com>
2019-12-03 11:18:46 -0600

    Add failing test cases

--- day2/src/intcode.spec.ts
+++ day2/src/intcode.spec.ts
@@ -1,8 +1,33 @@
-//import {  } from "src/intcode";
+import { processIntcode } from "src/intcode";
 
-describe('intcode', function() {
-  it(`should do nothing`, function() {
-    result = 1;
-    expect(result).toBe(1);
-  });
+const testData = [
+  {
+    input: [1,9,10,3,2,3,11,0,99,30,40,50],
+    correctOutput: [3500,9,10,70,2,3,11,0,99,30,40,50]
+  },
+  {
+    input: [1,0,0,0,99],
+    correctOutput: [2,0,0,0,99]
+  },
+  {
+    input: [2,3,0,3,99],
+    correctOutput: [2,3,0,6,99]
+  },
+  {
+    input: [2,4,4,5,99,0],
+    correctOutput: [2,4,4,5,99,9801]
+  },
+  {
+    input: [1,1,1,4,99, 5,6,0,99],
+    correctOutput: [30,1,1,4,2,5,6,0,99]
+  }
+]
+
+describe('processIntcode', function() {
+  for (const testDatum of testData) {
+    it(`should correctly process ${testDatum.input}`, function() {
+      const result = processIntcode(testDatum.input);
+      expect(result).toEqual(testDatum.correctOutput);
+    });
+  }
 });
---
+++ day2/src/intcode.ts
@@ -0,0 +1,3 @@
+export function processIntcode(intcode: number[]): number[] {
+  return [2,0,0,0,99];
+}
