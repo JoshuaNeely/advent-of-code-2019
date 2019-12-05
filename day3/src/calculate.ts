4f2d9e6b1c79ea04e36c63927f2b71649213db28
JoshuaNeely <joshua.neely@adtran.com>
2019-12-04 20:24:57 -0600

    Solve day3 part 1

--- day3/src/calculate.ts
+++ day3/src/calculate.ts
@@ -1,1 +1,10 @@
-import { calculateClosestIntersection } from "./manhattanWires";
+import { calculateClosestIntersectionDistance, Wire } from "./manhattanWires";
+import { readFileSync } from 'fs';
+
+const wire1Data = readFileSync('./src/wire1Data.txt', 'utf-8');
+const wire2Data = readFileSync('./src/wire2Data.txt', 'utf-8');
+
+const wire1: Wire = wire1Data.split(',');
+const wire2: Wire = wire2Data.split(',');
+
+console.log(calculateClosestIntersectionDistance(wire1, wire2));
--- day3/src/manhattanWires.ts
+++ day3/src/manhattanWires.ts
@@ -20,7 +20,21 @@ export interface OrthogonalSegment extends Segment {
 }
 
 export function calculateClosestIntersectionDistance(wire1: Wire, wire2: Wire): number {
-  return 123;
+  const allIntersectingCoordinates = getAllIntersectingCoordinates(wire1, wire2);
+
+  let smallestNonZeroDist = -1;
+  for (let coord of allIntersectingCoordinates) {
+    const segment = {
+      coordinate1: {x:0, y:0},
+      coordinate2: coord,
+      axis: Axis.X    // it doesn't matter
+    }
+    const dist = calculateManhattan(segment);
+    if (dist > 0 && (smallestNonZeroDist == -1 || dist < smallestNonZeroDist)) {
+      smallestNonZeroDist = dist;
+    }
+  }
+  return smallestNonZeroDist;
 }
 
 export function getAllIntersectingCoordinates(
